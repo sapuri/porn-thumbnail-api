0736db97a67fdfae1b1383b6c46722b5b9ccad89
m1nam1 <info@minami.xyz>
2017-10-20 22:05:09 +0900

    Fix format

--- scraping/scraping.go
+++ scraping/scraping.go
@@ -1,45 +1,45 @@
 package scraping
 
 import (
-    "fmt"
-    "log"
-    "strings"
-    "regexp"
-    "github.com/PuerkitoBio/goquery"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"regexp"
+	"strings"
 )
 
 // Return PornHub thumbnail urls
 func Pornhub(url string) []string {
-    var thumbnail_urls []string
+	var thumbnail_urls []string
 
-    // HTML取得
-    doc, err := goquery.NewDocument(url)
-    if err != nil {
-        log.Printf("url scraping failed")
-        log.Fatal(err)
-    }
+	// HTML取得
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		log.Printf("url scraping failed")
+		log.Fatal(err)
+	}
 
-    // 動画プレイヤーを取得
-    player_script := doc.Find("#player").First().Children().First()
+	// 動画プレイヤーを取得
+	player_script := doc.Find("#player").First().Children().First()
 
-    // "image_url" 以降の文字列を取得
-    str := strings.Split(player_script.Text(), "\"image_url\"")[1]
+	// "image_url" 以降の文字列を取得
+	str := strings.Split(player_script.Text(), "\"image_url\"")[1]
 
-    // "\" を除去
-    str = strings.Replace(str, "\\", "", -1)
+	// "\" を除去
+	str = strings.Replace(str, "\\", "", -1)
 
-    // 最初のURLを取得
-    r := regexp.MustCompile(`https?://[-_.!~*\'()a-zA-Z0-9;/?:@&=+$,%#]+`)
-    image_url := r.FindStringSubmatch(str)[0]
+	// 最初のURLを取得
+	r := regexp.MustCompile(`https?://[-_.!~*\'()a-zA-Z0-9;/?:@&=+$,%#]+`)
+	image_url := r.FindStringSubmatch(str)[0]
 
-    // "/original/" より前のURLを取得
-    str = strings.Split(image_url, "/original/")[0]
+	// "/original/" より前のURLを取得
+	str = strings.Split(image_url, "/original/")[0]
 
-    // 1から16までの全てのサムネイルURLを格納
-    for i := 1; i <= 16; i++ {
-        thumbnail_url := fmt.Sprintf("%s/original/%d.jpg", str, i)
-        thumbnail_urls = append(thumbnail_urls, thumbnail_url)
-    }
+	// 1から16までの全てのサムネイルURLを格納
+	for i := 1; i <= 16; i++ {
+		thumbnail_url := fmt.Sprintf("%s/original/%d.jpg", str, i)
+		thumbnail_urls = append(thumbnail_urls, thumbnail_url)
+	}
 
-    return thumbnail_urls
+	return thumbnail_urls
 }
--- server.go
+++ server.go
@@ -1,53 +1,53 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "github.com/ant0ine/go-json-rest/rest"
-    "github.com/m1nam1/porn-thumbnail-api/scraping"
+	"fmt"
+	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/m1nam1/porn-thumbnail-api/scraping"
+	"log"
+	"net/http"
 )
 
 func main() {
-    api := rest.NewApi()
-    api.Use(rest.DefaultDevStack...)
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
 
-    router, err := rest.MakeRouter(
-        rest.Get("/pornhub", Pornhub),
-    )
+	router, err := rest.MakeRouter(
+		rest.Get("/pornhub", Pornhub),
+	)
 
-    if err != nil {
-        log.Fatal(err)
-    }
-    api.SetApp(router)
-    log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	api.SetApp(router)
+	log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
 }
 
 type Result struct {
-    Status string `json:"status"`
-    SiteName string `json:"site_name"`
-    Url string `json:"url"`
-    ThumbnailUrls []string `json:"thumbnail_urls"`
+	Status        string   `json:"status"`
+	SiteName      string   `json:"site_name"`
+	Url           string   `json:"url"`
+	ThumbnailUrls []string `json:"thumbnail_urls"`
 }
 
 func Pornhub(w rest.ResponseWriter, r *rest.Request) {
-    url := r.URL.Query().Get("url")
-    if url != "" {
-        fmt.Println(url)
+	url := r.URL.Query().Get("url")
+	if url != "" {
+		fmt.Println(url)
 
-        thumbnail_urls := scraping.Pornhub(url)
-        if thumbnail_urls == nil {
-            rest.NotFound(w, r)
-            return
-        }
+		thumbnail_urls := scraping.Pornhub(url)
+		if thumbnail_urls == nil {
+			rest.NotFound(w, r)
+			return
+		}
 
-        w.WriteJson(
-            Result {
-                Status: "ok",
-                SiteName: "PornHub",
-                Url: url,
-                ThumbnailUrls: thumbnail_urls,
-            },
-        )
-    }
+		w.WriteJson(
+			Result{
+				Status:        "ok",
+				SiteName:      "PornHub",
+				Url:           url,
+				ThumbnailUrls: thumbnail_urls,
+			},
+		)
+	}
 }
